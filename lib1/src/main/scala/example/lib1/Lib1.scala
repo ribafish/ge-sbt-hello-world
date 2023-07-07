e4924beebe03368e39fa511f1e736ea90996844c
Gasper Kojek <gkojek@gradle.com>
2023-07-07 09:27:37 +0200

    Added usage of lib1 in hello and lib2 in lib1 - For me it doesn't compile with error Hello.scala:7:9: not found: value Lib1

--- lib1/src/main/scala/example/lib1/Lib1.scala
+++ lib1/src/main/scala/example/lib1/Lib1.scala
@@ -1,8 +1,10 @@
 package example.lib1
 
+import example.lib2.Lib2
+
 class Lib1 {
     def print() {
         println("Hello from lib1")
-//        Lib2.print()
+        Lib2.print()
     }
 }
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,10 @@
 package example
 
+import example.lib1.Lib1
+
 object Hello extends Greeting with App {
     println(greeting)
+    new Lib1.print()
 }
 
 trait Greeting {
